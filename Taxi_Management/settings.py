4fb2c2c741eff4e46e4d5fd939c4bc040deb37ff
Anubhav <choubey.anubhav253@gmail.com>
2024-06-26 19:04:32 +0530

    Added Basic Login Signup skelaton

--- Taxi_Management/settings.py
+++ Taxi_Management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4!u(21bq=q2p$2g24z#@f7vt&zwcg&jb4xh#47tebg+l*0x-j#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- Taxi_Management/views.py
+++ Taxi_Management/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request,"index.html")
@@ -6,5 +7,15 @@ def index(request):
 def login(request):
     return render(request,'login.html')
 
+@csrf_exempt
+def login_verify(request):
+    print("Login Verify")
+    return render(request,'login.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+@csrf_exempt
 def register(request):
+    print("Register")
     return render(request,'signup.html')
